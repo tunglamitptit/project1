f282545279b213a6b64ea5b48ef693a4b3e6d67a
= <=>
2020-06-04 17:47:43 +0700

    update 3

--- 1.js
+++ 1.js
@@ -1,27 +1,33 @@
-document.addEventListener("DOMContentLoaded",function(){
-var nut = document.querySelectorAll('div.nut ul li');
-var slides = document.querySelectorAll('div.slide div');
-for(var i = 0 ; i < nut.length; i++){
-nut[i].addEventListener('click',function(){
-    var nutnay = this;
-    var vitrislide = 0;
-    console.log(nutnay.previousElementSibling);
-    for(var i = 0;nutnay = nutnay.previousElementSibling; vitrislide++){
-        //chay den khi nut nay  = null thi dung.
-        // chay xong lenh nay khi click vao nut ta lay dc vitrislide
-    }
-    for(var i = 0; i < slides.length; i++){
-        slides[i].classList.remove('ra');
-    }
-    for(var i = 0; i < slides.length; i++){
-        slides[vitrislide].classList.add('ra');
-    }
-})
+// $(document).ready(function() {
+    
+// });
+// function slideShow() {
+    
+// }
+document.addEventListener("DOMContentLoaded",function() {
+    var nut = document.querySelectorAll('div.nut ul li');
+    var slides = document.querySelectorAll('div.slide div');
+    for(var i = 0 ; i < nut.length; i++){
+    nut[i].addEventListener('click',function(){
+        var nutnay = this;
+        var vitrislide = 0;
+        console.log(nutnay.previousElementSibling);
+        for(var i = 0;nutnay = nutnay.previousElementSibling; vitrislide++) {
+            //chay den khi nut nay  = null thi dung.
+            // chay xong lenh nay khi click vao nut ta lay dc vitrislide
+        }
+        for(var i = 0; i < slides.length; i++) {
+            slides[i].classList.remove('ra');
+        }
+        for(var i = 0; i < slides.length; i++) {
+            slides[vitrislide].classList.add('ra');
+        }
+    })
 }
 // Click chuyen slide
  
-    auto();
-    function auto(){
+auto();
+function auto() {
     var thoigian = setInterval(function(){
         var slide = document.querySelector('div.slide div.ra');
         var vitrislide = 0;
