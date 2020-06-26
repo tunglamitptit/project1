a6479b256edb97124080a1427661d6d05d5eeb7d
= <=>
2020-06-26 10:20:21 +0700

    update register.php

--- connect.php
+++ connect.php
@@ -11,15 +11,4 @@
     if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
     }
-    // $result = $conn->query("SELECT * FROM user");
-
-    // if ($result->num_rows > 0) {
-    //   // output data of each row
-    //   while($row = $result->fetch_assoc()) {
-    //     echo "id: " . $row["ID"]. " - Name: " . $row["Username"]. " - Class: " . $row["Pass"]. "<br>";
-    //   }
-    // } else {
-    //   echo "0 results";
-    // }
-    // $conn->close();
 ?>
--- register.php
+++ register.php
@@ -1,55 +1,46 @@
 <?php
-    // header("location:register.php");
     include "connect.php";
-    $nameErr = '';
-    if (isset($_POST["submit"])) {
-        $nameErr = '123123123123123';
-		// lấy thông tin từ các form bằng phương thức POST
-		$username = $_POST["username"];
+    // header("location:register.php");
+    $nameErr =  $emailErr = $passErr = $repassErr = "";
+    $username = $password = $re_password = $email = "";
+    $check = true;
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // lấy thông tin từ các form bằng phương thức POST
+        $username = $_POST["username"];
         $password = $_POST["password"];
         $re_password = $_POST["re_password"];
-		$email = $_POST["email"];
-		$result = $conn->query("SELECT * FROM users");
-		$rows_users = array(
-			"user_name" => array(), "e_mail" => array()
-		); 
-		$i = 0;
-		$check_1 = true;
-		$check_2 = true;
-		while ($row = $result->fetch_assoc()) {
-			$rows_users["user_name"][$i] = $row["Username"];
-			$rows_users["e_mail"][$i] = $row["Email"];
-			$i++;
-		}
-		for ($j = 0; $j <= $i; $j++) {
-			if ($username == $rows_users["user_name"][$j]) {
-				$check_1 = false;
-				break;
-			}
-			if ($email == $rows_users["e_mail"][$j]) {
-				$check_2 = false;
-				break;
-			}
-		}
-		//Kiểm tra điều kiện 
-		if ($username == "" || $password == "" || $re_password == "" || $email == "") {
-			echo "<script language='javascript'>alert('Bạn vui lòng nhập đầy đủ thông tin!'); window.location='register.php';</script>";
-		} else if ($password != $re_password) {
-			echo "<script language='javascript'>alert('Mật khẩu không trùng khớp!'); window.location='register.php';</script>";
-		} else if (!$check_1) {
-			echo "<script language='javascript'>alert('Tên tài khoản đã được sử dụng!'); window.location='register.php';</script>";
-		} else if (!$check_2) {
-			echo "<script language='javascript'>alert('Email đã được sử dụng!'); window.location='register.php';</script>";
-		} else {
-			$password = md5($password);
-			$sql = "INSERT INTO users (Username, Password, Email) VALUES ('$username', '$password', '$email')";
-			// thực thi câu $sql với biến conn lấy từ file connect.php
-			mysqli_query($conn, $sql);
-			echo "<script language='javascript'>alert('Đăng ký thành công!'); window.location='register.php';</script>";
-		}
-	}
+        $email = $_POST["email"];
+        $result_name = $conn->query("SELECT * FROM users WHERE Username='$username'");
+        $result_email = $conn->query("SELECT * FROM users WHERE Email='$email'");
+        //Kiểm tra điều kiện  
+        if ($username == "" || $password == "" || $re_password == "" || $email == "") {
+            $nameErr = "Username is required";
+            $passErr = "Password is required";
+            $emailErr = "Email is required";
+            $check = false;
+        }       
+        if ($password != $re_password) {
+            $repassErr = "Password didn't match";
+            $check = false;
+        }  
+        if ($result_name->num_rows > 0) {
+            $nameErr = "Username available";
+            $check = false;
+        }  
+        if ($result_email->num_rows > 0) {
+            $emailErr = "Email available";
+            $check = false;
+        }        
+        if ($check) {
+            $password = md5($password);
+            $sql = "INSERT INTO users (Username, Password, Email) VALUES ('$username', '$password', '$email')";
+            // thực thi câu $sql với biến conn lấy từ file connect.php
+            mysqli_query($conn, $sql);
+            echo "<script language='javascript'>alert('Đăng ký thành công!');</script>";
+        }
+    }
 
-	// $result = $conn->query("SELECT * FROM users");
+    // $result = $conn->query("SELECT * FROM users");
 
     // if ($result->num_rows > 0) {
     //   // output data of each row
@@ -64,10 +55,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">   
-        <link rel="stylesheet" type="text/css" href="./1.css">
+        <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge">    -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+        <link rel="stylesheet" type="text/css" href="./1.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Đăng ký thành viên</title>
@@ -115,26 +106,28 @@
                 </ul>
             </div>
         </div>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" class="register">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
             <div class="container">
                 <div>
                     <label>Username: </label>
-                    <input type="text" name="username" class="form-control">
-                    <span class="error">* <?php echo $nameErr;?></span>
+                    <input type="text" name="username" class="form-control" value="<?php echo $username;?>">
+                    <span class="error" style="color: #ff0000"><?php echo $nameErr; $nameErr ="";?></span>
                 </div>                  
                 <div>
                 </div>
                     <label>Password: </label>
                     <input type="password" name="password" class="form-control">
+                    <span class="error"><?php echo $passErr;?></span>
                 <div>
                     <label>Repassword: </label>
-                    <input type="password" name="re_password" class="form-control">
-                </div>
-                    
+                    <input type="password" name="re_password" class="form-control" >
+                    <span class="error"><?php echo $repassErr;?></span>
+                </div>                   
                 <div>
                 </div>
                     <label>Email: </label>
-                    <input type="email" name="email" class="form-control">
+                    <input type="email" name="email" class="form-control" value="<?php echo $email;?>">
+                    <span class="error"><?php echo $emailErr;?></span>
                 <div>
                     <button type="submit" name="submit" class="btn btn-primary">Register</button>
                 </div>   
