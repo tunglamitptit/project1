53b1eaf9a8552d96ef3a0c50503ed93f8164eca5
= <=>
2020-06-26 18:35:12 +0700

    update login

---
+++ login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge">    -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+        <link rel="stylesheet" type="text/css" href="./login_logout.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+        <title>Đăng nhập</title>
+    </head>
+    <body>
+        <div id="action_bar">
+            <div class="container">
+                <ul class="contact_details">
+                    <li class="slogan">Have any question?</li>
+                    <li class="phone">
+                        <i class="fa fa-phone" style="color: #adadad"></i>
+                        <a href="#">+84 397 561 148</a>
+                    </li>
+                    <li class="email">
+                        <i class="fa fa-envelope" style="color: #adadad"></i>
+                        <a href="#">tunglamldk@gmail.com</a>
+                    </li>
+                </ul>
+                <ul class="social">
+                    <li class="skype">
+                        <a href="#" title="Skype">
+                            <i class="fa fa-skype"></i>
+                        </a>
+                    </li>
+                    <li class="facebook">
+                        <a href="#" title="Facebook">
+                            <i class="fa fa-facebook"></i>
+                        </a>
+                    </li>
+                    <li class="twitter">
+                        <a href="#" title="Twitter">
+                            <i class="fa fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li class="instagram">
+                        <a href="#" title="Instagram">
+                            <i class="fa fa-instagram"></i>
+                        </a>
+                    </li>
+                    <li class="youtube">
+                        <a href="#" title="YouTube">
+                            <i class="fa fa-youtube"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </body> 
+</html>
