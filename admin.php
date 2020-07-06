99675c1509b65383f65de04281ead8260fe244b3
= <=>
2020-07-06 23:37:57 +0700

    update ajax for admin

--- admin.php
+++ admin.php
@@ -37,10 +37,10 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
         <!-- <link rel="stylesheet" type="text/css" href="./home.css"> -->
         <link rel="stylesheet" type="text/css" href="./admin.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="./1.js"></script>
+        <script type="text/javascript" src="../jQuery v3.5.1.js"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">     
+        
     </head>
     <body>
         <!-- <div id="action_bar">
@@ -85,22 +85,50 @@
                 </ul>
             </div>
         </div> -->
-        <div style="position: absolute;">
-            <ul class="nav flex-column">
-                <li class="nav-header">Admin menu</li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="product_listing.php">Quan ly san pham</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Disabled</a>
-                </li>
-            </ul>
-        </div>     
+        <div style="background-color: #333333;">
+            <div class="container">
+                <div class="navbar">
+                    <div class="navbar-nav" style="color: #ffffff; display: inline-block;">Hello <a href="#">Admin</a></div>
+                    <!-- <div class="navbar-nav">Logout</div> -->
+                </div>
+            </div>
+        </div>
+        <div style="padding: 10px 40px; width: 100%;" class="row">
+            <div class="col-sm-2">
+                <ul class="nav flex-column" style="width: 100%">
+                    <li class="nav-item header text-center">Admin menu</li>
+                    <li class="nav-item">
+                        <a class="nav-link action" href="#">Tin tuc</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link action" href="product_listing.php">Quan ly san pham</a>           
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link action" href="#">Don hang</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link action" href="#">User</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-sm-10" id="content"></div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function() {             
+                $(".action").click(function() {
+                    var x = $(this).html();
+                    if (x === "Quan ly san pham") {
+                        $("#content").load("product_listing.php");              
+                    } else if (x === "Tin tuc") {
+                        $("$content").load("product_listing.php")
+                    } else if (x === "Don hang") {
+                        $("$content").load("product_listing.php")
+                    } else if (x === "User") {
+                        $("$content").load("product_listing.php")
+                    }
+                });
+            });
+        </script>     
     </body>
 </html>
